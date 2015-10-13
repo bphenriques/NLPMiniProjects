9f52da07fb17fea3b70270668fd59040e6899dde
Tiago Santos <tiagomanuelfs@hotmail.com>
2015-10-13 15:01:17 +0100

    [ADD] MyAvalia added

--- Project1/AnnotatedCheck.py
+++ Project1/AnnotatedCheck.py
@@ -4,7 +4,18 @@ from Normalizer import normalizestring
 
 def myAvalia(annotationFile, questionsFile):
 
-    return accuracy(list())
+    answerslist = list()
+
+    open(questionsFile)
+    with open(questionsFile) as file:
+        for line in file:
+            question = line
+            answer = get_answer(line)
+            #Todo: not opening the file on every iteration
+            annotation = getannotation(line, answer, annotationFile, 20)
+            answerslist.append(annotation)
+
+    return accuracy(answerslist)
 
 
 def getannotation(question, answer, filepath, nanswers):
@@ -29,16 +40,14 @@ def getannotation(question, answer, filepath, nanswers):
 
 def accuracy(answers):
     tp = 0
-    fp = 0
-    tn = 0
-    fn = 0
+
 
     for x in answers:
         if(x == 'y'):
             tp = tp + 1
-        elif (x == 'n' | x== 'm'):
-            fp = fp +1
-    return 1 #accuracy perfeita ó
 
-
+    return float(tp) / len(answers) #accuracy perfeita ó
+#ibanana = list("ynmym")
+#banana = ['y', 'n', 'm']
+print accuracy(banana)
 #print getannotation("És aluno do Técnico?", "Isso é a coisa mais burra que disse desde há algum tempo.", "../AnotadoAll.txt", 20)

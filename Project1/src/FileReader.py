81f2389f766a707756fb99245c37d7e14a20af56
Bruno Henriques <brunoaphenriques@gmail.com>
2015-10-13 17:00:34 +0100

    Renamed RegexUtil; Improved FileReader with map

--- Project1/src/FileReader.py
+++ Project1/src/FileReader.py
@@ -3,16 +3,18 @@
 import re
 from RegexUtil import RegexUtil
 
-class QuestionsAnswerReader:
+class TriggersAnswerReader:
     __TRIGGER_TAG = "T"
     __ANSWER_TAG = "A"
-
-    fileName = ""
     __trigger_regex = ""
     __answer_regex = ""
+    __questions_answers_map = {}
 
-    def __init__(self, fileName):
-        self.fileName = fileName
+    file_name = None
+
+    ''' Init, receives file_name '''
+    def __init__(self, file_name):
+        self.file_name = file_name
 
         rew = RegexUtil()
         self.trigger_tag = rew.multiple_white_space() + self.__TRIGGER_TAG + rew.multiple_white_space() + "-" + rew.multiple_white_space()
@@ -20,69 +22,136 @@ class QuestionsAnswerReader:
         self.__trigger_regex = self.trigger_tag + rew.at_least_one(rew.anything)
         self.__answer_regex = self.answer_tag + rew.at_least_one(rew.anything)
 
-    def processFile(self):
-        fileIn = open(self.fileName, 'rU')
-        self._getTriggersAndAnswers(fileIn)
+    ''' Reads file given by input and stores the associated triggers and the set of answers '''
+    def process_file(self):
+        fileIn = open(self.file_name, 'rU')
+        self._get_triggers_and_answers(fileIn)
         fileIn.close()
-        return map
 
-    # Stores T and A into a map
-    def _getTriggersAndAnswers(self, fileIn):
+    ''' Print internal representation of map of triggers and answers'''
+    def dump_map(self):
+        print("Result: ", self.__questions_answers_map)
+
+
+    ''' Reads the file 2 lines at a time and processes the corresponding trigger and answer if exists using regex '''
+    def _get_triggers_and_answers(self, file_in):
         while True:
+            possible_trigger = file_in.readline()
+            if not possible_trigger: break
+            possible_answer = file_in.readline()
+            if not possible_answer: break
+
             # Look for "T - Something?"
-            trigger = self._readLine(fileIn.readline(), self.__trigger_regex)
-            if len(trigger) < 0:
-                break
+            trigger = self._read_trigger(possible_trigger)
+            if len(trigger) == 0:
+                continue
 
-            possibleAnswer = fileIn.readline()
-            if not possibleAnswer: break  # EOF
-            answer = self._readLine(possibleAnswer, self.__answer_regex)
-            if len(answer) < 0:
-                break
+            # Look for "A - Something..."
+            answer = self._read_answer(possible_answer)
+            if len(answer) == 0:
+                continue
 
-            if __debug__:
-                if len(trigger) > 0:
-                    print "Trigger: ", trigger
-                if len(answer) > 0:
-                    print "Answer: ", answer
+            #processing triggers and answers as strings
+            self._process_trigger_answer(''.join(trigger), ''.join(answer))
 
-    def _readLine(self, possibleTrigger, regex):
-        return re.findall(regex, possibleTrigger)
+    ''' Update internal map of triggers and answers'''
+    def _process_trigger_answer(self, trigger, answer):
+        #Update internal map of triggers and answers
+        self._put(self.__trigger_normalizer(trigger), answer)
 
-    def get_answer(self, question):
+    '''Adds element to the map, if the key already exists, append the value to the existing ones'''
+    def _put(self, trigger, response):
+        #if __debug__:
+        #    print "Adding ", response, " into ", " trigger ", trigger
+
+        if trigger not in self.__questions_answers_map:
+            self.__questions_answers_map[trigger] = []
+
+        self.__questions_answers_map[trigger] += [response]
+
+    def __trigger_normalizer(self, trigger):
+        return trigger
+
+    def _read_trigger(self, possible_trigger):
+        return re.findall(self.__trigger_regex, possible_trigger)
+
+    def _read_answer(self, possible_answer):
+        return re.findall(self.__answer_regex, possible_answer)
+
+    '''Number of triggers'''
+    def number_triggers(self):
+        return len(self.__questions_answers_map)
+
+    '''Returns all answers given a question'''
+    def get_answers(self, question):
+        return self.__questions_answers_map[question]
+
+    '''Returns the most probable trigger'''
+    def get_answer(self, trigger):
         return "Gosto de bananas"
 
-
 #For testing
-class TestQuestionsAnswerReader(QuestionsAnswerReader):
-    def testTriggerReader(self, possibleTrigger):
-        return self.__readLine(possibleTrigger, self.__trigger_regex)
+class TestTriggersAnswerReader(TriggersAnswerReader):
+    __test_triggers = [
+        " T - És mesmo parolo!",
+        " T - Eu vou à loja do mestra André. É mesmo aqui ao lado!",
+        " T - ÀàÁáÉéèÉÈíÍìÌÓóòÒãõôÔÀÀÀAÀÀÀÀÀÀÀ. Derp. Não estou a dizer coisa com coisa (estou?)...",
+        " T - E então ele disse: \"Cenas engraçadas",
+        " T - E então ele disse: \"Dás me o teu número?",
+        " T - Estou preguiçoso. Vou escrever mal. Queres ìr alìh? È que...è que ? Sìgh!"
+    ]
 
-    def testAnswerReader(self, possibleAnswer):
-        return self.__readLine(possibleAnswer, self.__answer_regex)
+    __test_answers = [
+        " A - És mesmo parolo!",
+        " A - Eu vou à loja do mestra André. É mesmo aqui ao lado!",
+        " A - ÀàÁáÉéèÉÈíÍìÌÓóòÒãõôÔÀÀÀAÀÀÀÀÀÀÀ. Derp. Não estou a dizer coisa com coisa (estou?)...",
+        " A - E então ele disse: \"Cenas engraçadas",
+        " A - E então ele disse: \"Dás me o teu número?",
+        " A - Estou preguiçoso. Vou escrever mal. Queres ìr alìh? È que...è que ? Sìgh!"
+    ]
 
-    def testTriggerRegex(self):
-        frasesManhosas = [
-            " T - És mesmo parolo!",
-            " T - Eu vou à loja do mestra André. É mesmo aqui ao lado!",
-            " T - ÀàÁáÉéèÉÈíÍìÌÓóòÒãõôÔÀÀÀAÀÀÀÀÀÀÀ. Derp. Não estou a dizer coisa com coisa (estou?)...",
-            " T - E então ele disse: \"Cenas engraçadas",
-            " T - E então ele disse: \"Dás me o teu número?",
-            " T - Estou preguiçoso. Vou escrever mal. Queres ìr alìh? È que...è que ? Sìgh!"
-        ]
-
-        qar = QuestionsAnswerReader("")
-
+    def test_answer_regex(self):
         count = 0
-        for frase in frasesManhosas:
-            result = qar.testTriggerReader(frase)
-            print result
+        for answer in self.__test_answers:
+            result = self._read_answer(answer)
             if len(result) > 0:
                 count += 1
 
-        print "Got ", count, " correct out of ", len(frasesManhosas)
+        assert(len(self.__test_answers) == count)
+        print "Passed ", count, " tests reading answers"
+
+    def test_trigger_regex(self):
+        count = 0
+        for trigger in self.__test_triggers:
+            result = self._read_trigger(trigger)
+            if len(result) > 0:
+                count += 1
+
+        assert(len(self.__test_triggers) == count)
+        print "Passed ", count, " tests reading triggers"
+
+    def test_put(self):
+        self.put("Question1", "Response1")
+        assert(len(self.get_answers("Question1")) == 1)
+
+        self.put("Question2", "Response1")
+        self.put("Question2", "Response2")
+        assert(len(self.get_answers("Question2")) == 2)
+
+        print("Passed Map of questions and responses")
+
+    def test_process_file(self):
+        self.process_file()
+        assert self.number_triggers() > 0
+        print "Passed reading file. Detected some triggers, the other tests covered the rest"
+
 
 
 if __name__ == '__main__':
-    questions_answer_reader = TestQuestionsAnswerReader("src/TestResources/PerguntasPosSistema.txt")
-    questions_answer_reader.processFile()
+    questions_answer_reader = TestTriggersAnswerReader("src/TestResources/PerguntasPosSistema.txt")
+
+    print "--- STARTING TESTS ---"
+    questions_answer_reader.test_trigger_regex()
+    questions_answer_reader.test_answer_regex()
+    questions_answer_reader.test_process_file()
+    print "--- END OF TESTS ---"

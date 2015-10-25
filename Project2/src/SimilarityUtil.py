f909bb0e7b711d5c751ef2f025cb701517b271c0
Tiago Santos <tiagomanuelfs@hotmail.com>
2015-10-25 12:24:22 +0000

    Tests for MED words added

--- Project2/src/SimilarityUtil.py
+++ Project2/src/SimilarityUtil.py
@@ -1,3 +1,4 @@
+
 def jaccard(v1, v2):
     s1, s2 = set(v1), set(v2)
     intersection = s1.intersection(s2)
@@ -11,3 +12,40 @@ def dice(v1, v2):
 
 def word_to_list_chars(word):
     return list(word)
+
+def MED(sentence1, sentence2, c1=1, c2=1, c3=1):
+    size1, size2 = len(sentence1), len(sentence2)
+
+    if size1 == 0:
+        return size2
+    if size2 == 0:
+        return size1
+
+    matrix_row_size = size1 + 1
+    matrix_col_size = size2 + 1
+
+    #init size
+    matrix = [None] * matrix_row_size
+    for i in range(matrix_row_size):
+        matrix[i] = [None] * matrix_col_size
+
+    #init first row
+    for i in range(matrix_row_size):
+        matrix[i][0] = i
+
+    #init first column
+    for j in range(matrix_col_size):
+        matrix[0][j] = j
+
+    for i in range(1, matrix_row_size):
+        word1 = sentence1[i - 1]
+        for j in range(1, matrix_col_size):
+            word2 = sentence2[j - 1]
+            if word1 == word2:
+                matrix[i][j] = matrix[i-1][j-1]
+            else:
+                matrix[i][j] = min(matrix[i-1][j] + c1, matrix[i][j-1] + c2, matrix[i-1][j-1] + c3)
+
+    return matrix[matrix_row_size - 1][matrix_col_size - 1]
+
+
--- Project2/src/SimilarityUtilTests.py
+++ Project2/src/SimilarityUtilTests.py
@@ -15,11 +15,26 @@ def test_dice():
     assert dice(s, t) == float(10)/float(13)
     assert dice(s, w) == float(6)/float(10)
 
+def test_med():
+    s = 'batalha'
+    t = 'barata'
+    u = 'Bruno'
+    w = 'bruno'
+    y = 'Bruna'
+    x = 'Brunoob'
+
+    assert MED(s, t) == 3
+    assert MED(t, s) == 3
+    assert MED(u, w) == 1
+    assert MED(u, y) == 1
+    assert MED(x, u) == 2
+    assert MED(x, w) == 3
 
 if __name__ == "__main__":
     tests = [
         test_jaccard,
-        test_dice
+        test_dice,
+        test_med
     ]
 
     run_tests(tests)
